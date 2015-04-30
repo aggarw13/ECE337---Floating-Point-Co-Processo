1f779115a488f454f1cf47ac2dcaa513fc838135
Mentor Graphics <mg76@ecegrid-thin4.ecn.purdue.edu>
2015-04-30 17:08:16 -0400

    added the test bench file

---
+++ source/tb_Floating_point_co_processor.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   tb_Floating_point_co_processor.sv
+// Created:     4/30/2015
+// Author:      James Alliger
+// Lab Section: 337-05
+// Version:     1.0  Initial Design Entry
+// Description: The main test bench for the floating point co-processor
