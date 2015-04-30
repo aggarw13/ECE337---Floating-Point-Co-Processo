22bf752e30dcd44f39ac1a65ec988fbe00c0988e
Mentor Graphics <mg76@ecegrid-thin4.ecn.purdue.edu>
2015-04-30 15:54:30 -0400

    added in adder to the top level diagram

--- work/@floating_point_co_processor/_primary.vhd
+++ work/@floating_point_co_processor/_primary.vhd
@@ -2,7 +2,8 @@ library verilog;
 use verilog.vl_types.all;
 entity Floating_point_co_processor is
     generic(
-        INSTRUCT_WIDTH  : integer := 16
+        INSTRUCT_WIDTH  : integer := 16;
+        ALU_BLOCKS      : integer := 6
     );
     port(
         clk             : in     vl_logic;
@@ -10,4 +11,5 @@ entity Floating_point_co_processor is
     );
     attribute mti_svvh_generic_type : integer;
     attribute mti_svvh_generic_type of INSTRUCT_WIDTH : constant is 1;
+    attribute mti_svvh_generic_type of ALU_BLOCKS : constant is 1;
 end Floating_point_co_processor;
